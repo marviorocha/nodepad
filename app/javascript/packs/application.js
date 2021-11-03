bcf617c5618d73d3c1102395939093793bbf159c
Marvio Rocha <marviorocha@marviorocha.com>
2021-11-03 16:27:12 -0300

    Added new version Ruby
    Update papeline

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
-import 'bootstrap'
-import "../stylesheet/application"
 import Rails from "@rails/ujs"
+import 'bootstrap'
+import "../stylesheets/style"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "controllers"
